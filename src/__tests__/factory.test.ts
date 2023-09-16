12341c3046ba75dcfdd8aca7c96efc210e603df5
yifanwww <yifanw1101@gmail.com>
2023-09-16 17:46:43 +0800

    feat: add examples and tests for Ok and Err

--- src/__tests__/factory.test.ts
+++ src/__tests__/factory.test.ts
@@ -1,13 +1,26 @@
 import { Err, Ok } from '../factory';
+import type { Result } from '../types';
 
 describe(`Test fn \`${Ok.name}\``, () => {
     it('should create `Ok` result', () => {
-        expect(Ok(1)).toMatchSnapshot();
+        const result1 = Ok(1);
+        const result2 = Ok<number, string>(1);
+        const result3: Result<number, string> = Ok(2);
+
+        expect(result1).toMatchSnapshot();
+        expect(result2).toMatchSnapshot();
+        expect(result3).toMatchSnapshot();
     });
 });
 
 describe(`Test fn \`${Err.name}\``, () => {
     it('should create `Err` result', () => {
-        expect(Err('Some error message')).toMatchSnapshot();
+        const result1 = Err('Some error message');
+        const result2 = Err<number, string>('Some error message');
+        const result3: Result<number, string> = Err('Some error message');
+
+        expect(result1).toMatchSnapshot();
+        expect(result2).toMatchSnapshot();
+        expect(result3).toMatchSnapshot();
     });
 });
--- src/factory.ts
+++ src/factory.ts
@@ -3,10 +3,21 @@ import type { Result } from './types';
 
 /**
  * Creates a `Result` that contains the success value.
+ *
+ * Examples:
+ * ```ts
+ * const result = Ok(1);
+ * ```
  */
 export function Ok<T>(value: T): Result<T, never>;
 /**
  * Creates a `Result` that contains the success value.
+ *
+ * Examples:
+ * ```ts
+ * const result1 = Ok<number, string>(1);
+ * const result2: Result<number, string> = Ok(2);
+ * ```
  */
 export function Ok<T, E>(value: T): Result<T, E>;
 export function Ok<T, E>(value: T): Result<T, E> {
@@ -15,10 +26,21 @@ export function Ok<T, E>(value: T): Result<T, E> {
 
 /**
  * Creates a `Result` that contains the error value.
+ *
+ * Examples:
+ * ```ts
+ * const result = Err('Some error message');
+ * ```
  */
 export function Err<E>(error: E): Result<never, E>;
 /**
  * Creates a `Result` that contains the error value.
+ *
+ * Examples:
+ * ```ts
+ * const result1 = Err<number, string>('Some error message');
+ * const result2: Result<number, string> = Err('Some error message');
+ * ```
  */
 export function Err<T, E>(error: E): Result<T, E>;
 export function Err<T, E>(error: E): Result<T, E> {
