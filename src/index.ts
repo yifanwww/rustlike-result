39ec0c0fc3a56da98a6e6ec9fe8141b9a68b8d17
yifanwww <yifanw1101@gmail.com>
2024-11-09 23:52:51 +0800

    chore: delete old package code

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './factory';
-export * from './factoryAsync';
-export * from './json/simple';
-export * from './json/types';
-export * from './Result';
-export * from './ResultAsync';
-export * from './resultify';
